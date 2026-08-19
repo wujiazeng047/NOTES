4981e431c336f4914de08a4d14648dae8ceec467
wujiazeng047 <wujiazeng047@gmail.com>
2026-08-19 23:17:42 +0630

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lwin-car-info-notes-v3";
+const CACHE = "lwin-car-info-notes-v4";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./lwin-logo.png"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
